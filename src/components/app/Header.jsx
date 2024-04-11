ae25d2c4166e1f50690e4f211d2a67a902700c16
Shani <shanivish95@gmail.com>
2024-04-11 14:04:12 +0530

    responsive completed

--- src/components/app/Header.jsx
+++ src/components/app/Header.jsx
@@ -1,18 +1,25 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
+import { useState } from 'react'
+
 import logo from '../../../public/logo.png'
 import { CgProfile } from 'react-icons/cg'
 import { IoIosCart } from 'react-icons/io'
 import MenuDropdown from '../ui/MenuDropdown'
 import ShopDropdown from '../ui/ShopDropdown'
+import { IoClose } from 'react-icons/io5'
+import { IoMenu } from 'react-icons/io5'
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <div>
+    <div className=''>
       {/* *******1******* */}
       <div className='bg-orange '>
         <div className='h-14 md:px-10  flex justify-between items-center text-white'>
-          <h1 className='text-xs ml-5 md:ml-0 md:text-lg'>
+          <h1 className='text-xs ml-5 md:ml-0 md:text-base'>
             Get 20%{' '}
             <span className='font-semibold text-xs md:text-lg'>
               Flat Discount
@@ -27,7 +34,7 @@ const Header = () => {
         </div>
       </div>
       {/* ********2********* */}
-      <div className='flex bg-white md:px-10 md:justify-between h-20 py-2 items-center'>
+      <div className='flex bg-white md:px-10 justify-between h-20 py-2 items-center'>
         <div className='flex items-center gap-1'>
           <Image src={logo} width={50} height={50} alt='logo' />
           <h1 className='text-lg'>
@@ -74,49 +81,98 @@ const Header = () => {
           <a href='/login'>
             <CgProfile className='w-8 h-8' />
           </a>
-          <div className='flex gap-3 items-center bg-light_gray p-2 rounded-lg'>
-            <a href='/cart'>
+          <a href='/cart'>
+            <div className='flex gap-3 items-center bg-light_gray p-2 rounded-lg'>
               {' '}
               <IoIosCart className='w-6 h-6' />{' '}
-            </a>
-            <div className=''>
-              <div className='flex justify-center items-center bg-orange  text-white rounded-full  h-4 w-8'>
-                0
+              <div className=''>
+                <div className='flex justify-center items-center bg-orange  text-white rounded-full  h-4 w-8'>
+                  0
+                </div>
+                <p>My Cart</p>
               </div>
-              <p>My Cart</p>
             </div>
-          </div>
+          </a>
+        </div>
+        {/* *******menu button mobile******** */}
+        <div className='text-2xl mr-3 cursor-pointer md:hidden  '>
+          {!isOpen ? (
+            <button
+              onClick={() => {
+                setIsOpen(true)
+              }}
+            >
+              <IoMenu />
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setIsOpen(false)
+              }}
+            >
+              <IoClose />
+            </button>
+          )}
         </div>
       </div>
       <hr className='text-light_gray'></hr>
       {/* ************3******* */}
-      <div className='md:flex  hidden  px-10 justify-between items-center h-12'>
-        <div>
+      <div className='md:flex z-50 relative   md:bg-white  md:px-10 justify-between items-center h-12'>
+        <div className='hidden md:block'>
           <MenuDropdown />
         </div>
         {/* ******li-item****** */}
-        <div className='flex'>
-          <ul className='flex gap-6 text-light_black '>
-            <li className='hover:text-orange'>
+        <div
+          className={`md:flex bg-light_gray  md:bg-white h-screen md:h-auto ${
+            isOpen
+              ? '-translate-x-[0%] transition-all ease-in duration-[0.5s]'
+              : '-translate-x-[100%] md:translate-x-[0%] transition-all ease-in duration-[0.5s]'
+          }`}
+        >
+          <div className='block md:hidden'>
+            <div className='w-full flex justify-center  pt-5'>
+              <ul className='flex items-center gap-5'>
+                <li className='bg-white px-6 py-2 hover:bg-orange hover:text-white duration-1000 rounded-lg'>
+                  <a href='/login'>Login</a>
+                </li>
+
+                <li className='bg-white px-6 hover:bg-orange hover:text-white duration-1000 py-2 rounded-lg'>
+                  <a href='signUp'>SignUp</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <ul className='md:flex gap-6 ml-10 z-50 pt-6 md:pt-0 md:ml-0 text-black '>
+            <li className='hover:text-orange  text-xl md:text-base  pt-2 md:pt-0'>
               <a href='/'>HOME</a>
             </li>
-            <li className=''>
+            <li className='w-28 text-xl md:text-base pt-2 md:pt-0'>
               <ShopDropdown />
             </li>
-            <li className='hover:text-orange'>SPECIAL OFFER</li>
-            <li className='hover:text-orange'>NEW ARRIVALS</li>
-            <li className='hover:text-orange'>
+            <li className=' hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
+              SPECIAL OFFER
+            </li>
+            <li className='hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
+              NEW ARRIVALS
+            </li>
+            <li className='hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
               <a href='/about'>ABOUT US</a>
             </li>
-            <li className='hover:text-orange'>BLOG</li>
-            <li className='hover:text-orange'>FAQS</li>
-            <li className='hover:text-orange'>
-              <a href='/contact'>CONTACT US</a>
+            <li className='hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
+              BLOG
+            </li>
+            <li className='hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
+              FAQS
+            </li>
+            <li className='hover:text-orange  text-xl md:text-base  pt-3 md:pt-0'>
+              <a href='/contact' className='pt-3 md:pt-0'>
+                CONTACT US
+              </a>
             </li>
           </ul>
         </div>
         {/* ******* */}
-        <div className='text-light_black hover:text-orange'>
+        <div className='text-light_black hidden md:block hover:text-orange'>
           <h1>Need Help? Contact Us</h1>
         </div>
       </div>
