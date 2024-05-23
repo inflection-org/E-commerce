6c6721346f623f8999b132363121261b090d5b88
Shani <shanivish95@gmail.com>
2024-05-23 16:30:02 +0530

    some error fixed

---
+++ src/app/axios/Api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import { getCookie } from './CookieConfig'
+
+const instance = axios.create({
+  baseURL: 'http://localhost:5000',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getCookie('access_token')}`,
+  },
+  timeout: 10000,
+})
+
+export function setToken(t) {
+  instance.defaults.headers.common['Authorization'] = `Bearer ${t}`
+  console.log(instance.defaults)
+}
+
+export default instance
