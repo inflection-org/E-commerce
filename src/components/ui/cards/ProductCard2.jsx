7223bcca6584a6ef83b9f6f1eb5f062480fc0157
Shani <shanivish95@gmail.com>
2024-06-04 17:20:43 +0530

    data props passed

--- src/components/ui/cards/ProductCard2.jsx
+++ src/components/ui/cards/ProductCard2.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 
-const ProductCard2 = () => {
+const ProductCard2 = ({ name, dis, price, pic }) => {
   return (
     <div>
-      <div className='flex items-center bg-light_gray w-auto h-auto py-8 px-4 rounded-lg'>
+      <div className='flex items-center bg-light_gray w-auto h-[250px] py-8 px-4 rounded-lg'>
         <div className='ml-4 w-52'>
-          <p className='text-sm text-gray'>Watch</p>
-          <h1 className='text-2xl font-bold mt-3'>Headphone</h1>
-          <p className='text-sm mt-2'>
-            Starting at <span className='text-orange'>$399</span>
-          </p>
+          <p className='text-sm text-gray'>{name}</p>
+          <h1 className='text-lg font-semibold mt-3'>{dis}</h1>
           <button className='bg-white hover:bg-orange hover:text-white duration-1000 rounded-full px-4 md:px-8 py-3 mt-8 md:mt-14'>
             Shop Now
           </button>
         </div>
         <div className='w-52'>
-          {/* <img
+          <img
             className='object-cover group-hover:scale-110 rounded-md duration-1000 w-auto h-auto '
-            src={'/product/S3.jpg'}
+            src={pic}
             alt='logo'
-          /> */}
+          />
         </div>
       </div>
     </div>
