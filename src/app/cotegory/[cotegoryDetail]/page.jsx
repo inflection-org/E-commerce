2b20d98929aa418646cea4f2ff80b018ac2457fd
Shani <shanivish95@gmail.com>
2024-06-05 16:25:30 +0530

    daynamic cart page added

---
+++ src/app/cotegory/[cotegoryDetail]/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='px-5 md:px-10 w-full'>
+      <div className='flex justify-center items-center bg-light_gray h-96'>
+        <h1 className='text-xl font-bold'>Coming Soon...</h1>
+      </div>
+    </div>
+  )
+}
+
+export default page
