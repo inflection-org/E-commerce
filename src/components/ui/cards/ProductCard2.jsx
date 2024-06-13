23eed9aa4fb3aa94c096a7029a55d515e499153f
Adarsh Maurya <adarshmaurya2303@gmail.com>
2024-06-13 20:18:31 +0530

    Category card change (css)

--- src/components/ui/cards/ProductCard2.jsx
+++ src/components/ui/cards/ProductCard2.jsx
@@ -3,20 +3,20 @@ import React from "react";
 const ProductCard2 = ({ name, dis, price, pic, categoriesDetails }) => {
   return (
     <>
-      <div className="flex items-center bg-light_gray w-auto h-[250px] py-8 px-4 rounded-lg">
-        <div className="ml-4  w-52">
+      <div className="flex items-center bg-light_gray w-auto h-full sm:h-[250px] p-5 rounded-lg">
+        <div className="w-52 flex flex-col gap-3">
           <p className="text-sm text-gray">{name}</p>
-          <h1 className=" text-base md:text-lg font-semibold mt-3">{dis}</h1>
+          <h1 className=" text-base md:text-lg font-semibold">{dis}</h1>
           <button
             onClick={() => categoriesDetails()}
-            className="bg-white hover:bg-orange hover:text-white duration-1000 rounded-full px-4 md:px-8 py-3 mt-8 md:mt-14"
+            className="w-32 py-2 bg-white hover:bg-orange hover:text-white duration-1000 rounded-full border-2 hover:border-1 border-orange"
           >
             Shop Now
           </button>
         </div>
         <div className="w-52">
           <img
-            className="object-cover group-hover:scale-110 rounded-md duration-1000 w-auto h-auto "
+            className="object-cover group-hover:scale-110 rounded-md duration-1000 w-auto h-auto"
             src={pic}
             alt="logo"
           />
