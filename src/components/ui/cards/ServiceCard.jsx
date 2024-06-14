5c9e2b9d4b23eaae2a636fc9218c8f4b4355ac7a
Adarsh Maurya <adarshmaurya2303@gmail.com>
2024-06-14 11:27:27 +0530

    Card updated...

--- src/components/ui/cards/ServiceCard.jsx
+++ src/components/ui/cards/ServiceCard.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const ServiceCard = (props) => {
   return (
-    <div className=''>
-      <div className='border-2 group border-light_gray hover:shadow-lg rounded-lg h-auto w-auto'>
-        <div className='flex justify-center mt-5 '>{props.children}</div>
-        <div className='text-center  mt-5'>
-          <h1 className='group-hover:text-orange text-lg'>{props.title}</h1>
-          <p className='text-sm text-gray mt-2 mb-3'>{props.disc}</p>
+    <>
+      <div className="border-2 group border-pink rounded-lg h-auto w-auto">
+        <div className="flex justify-center mt-5 ">{props.children}</div>
+        <div className="text-center  mt-5">
+          <h1 className="text-lg">{props.title}</h1>
+          <p className="text-sm text-gray mt-2 mb-3">{props.disc}</p>
         </div>
       </div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
