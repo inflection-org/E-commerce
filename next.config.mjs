555074a71e50714046cc3ae5e6d773f038715881
Adarsh Maurya <adarshmaurya2303@gmail.com>
2024-06-25 10:36:33 +0530

    build error fixing....

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['storage.googleapis.com', 'https://t4.ftcdn.net'],
+    domains: ["storage.googleapis.com", "https://t4.ftcdn.net"],
   },
-}
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
